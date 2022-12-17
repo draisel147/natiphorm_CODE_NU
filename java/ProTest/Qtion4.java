a6f3706216f61d9bc56978ba9aaa2660d1904cef
draisel147 <rivaillena@gmail.com>
2022-12-17 15:10:13 +0700

    update code

--- java/ProTest/Qtion4.java
+++ java/ProTest/Qtion4.java
@@ -2,15 +2,16 @@ package ProTest;
 
 import java.util.Scanner;
 
+//โปรแกรม หารเลข
 public class Qtion4 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println();
         int n = scanner.nextInt();
-        System.out.println();
+
         int m = scanner.nextInt();
+        System.out.println();
 
         for (int i = 1; i <= n; i++) {
 
