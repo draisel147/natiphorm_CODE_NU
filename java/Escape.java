29c6fe303b304148a0d33e0f0777c7001d0bcef2
draisel147 <rivaillena@gmail.com>
2022-12-28 16:29:52 +0700

    add .class

--- java/Escape.java
+++ java/Escape.java
@@ -1,8 +1,9 @@
 public class Escape {
     public static void main(String[] args) {
         /*
-         * \n => ขึ้นบรรทัดใหม่
-         * \t => tap
+         * \n => ขึ้นบรรทัดใหม่ // ใช้บ่อย******
+         * \t => tap // ***********
+         * \b =>
          */
         System.out.println("Hello\nWorld");
         System.out.println("Hello\tWorld");
