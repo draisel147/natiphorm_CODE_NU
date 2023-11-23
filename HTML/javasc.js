09b062cf422c62785785b048d99fa463f2b0bec7
draisel147 <rivaillena@gmail.com>
2023-11-23 09:32:06 +0700

    update javascript and dart

--- HTML/javasc.js
+++ HTML/javasc.js
@@ -1,6 +1,9 @@
+// var ตาม้วยชื่อจะสร้างตัวแปรเอาไว้เก็บค่า
 var name = "neytiphorm"
+// console.log แสดงค่าที่ console (กด F12 เพื่อดู)
 console.log(name)
 
+//เมื่อสร้างตัวแปรชื่อนั้นแล้วไม่ต้องพิม var นำหน้าแต่พิมชื่อตัวแปรแล้วเราสามารถเปลี่ยนค่าในตัวแปรได้เลย
 name = "The ney"
 console.log(name)
 
@@ -10,8 +13,9 @@ console.log(firstname)
 var lastname = "sonta"
 console.log(lastname)
 
+// alert ให้มันเด้งขึ้น popup ก่อนเข้าเว็บไซร์
 alert(firstname+" "+lastname)
 var age = 10
-
+// typeof ตรวจสอบว่ามีค่าเป็นอะไร เช่น string int 
 console.log(typeof age)
  
